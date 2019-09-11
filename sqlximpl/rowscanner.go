2a0e1af7c445d2902b9a424f15ba31f8a68d1e6b
Erik Unger <erik@erikunger.com>
2019-09-11 15:01:54 +0200

    support pointers to nil struct pointers in sqlximpl.rowScanner.ScanStruct

--- sqlximpl/rowscanner.go
+++ sqlximpl/rowscanner.go
@@ -1,6 +1,10 @@
 package sqlximpl
 
-import "github.com/jmoiron/sqlx"
+import (
+	"reflect"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type rowScanner struct {
 	row *sqlx.Row
@@ -11,5 +15,20 @@ func (s rowScanner) Scan(dest ...interface{}) error {
 }
 
 func (s rowScanner) ScanStruct(dest interface{}) error {
+	if v := reflect.ValueOf(dest); v.Kind() == reflect.Ptr && !v.IsNil() {
+		v = v.Elem()
+		// sqlx StructScan does not support pointers to nil pointers
+		// so set pointer to newly allocated struct
+		if v.Kind() == reflect.Ptr && v.IsNil() {
+			n := reflect.New(v.Type().Elem())
+			err := s.row.StructScan(n.Interface())
+			if err != nil {
+				return err
+			}
+			v.Set(n)
+			return nil
+		}
+	}
+
 	return s.row.StructScan(dest)
 }
