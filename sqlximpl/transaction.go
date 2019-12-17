7400808bdac197f605974dc1810e157b771e6c4c
Erik Unger <erik@erikunger.com>
2019-12-17 17:33:14 +0100

    make sqlximpl.Connection and Transaction unexported again and add Connection() function to wrap pre existing sqlx.DB

--- sqlximpl/transaction.go
+++ sqlximpl/transaction.go
@@ -7,29 +7,29 @@ import (
 	"github.com/domonda/go-sqldb/implhelper"
 )
 
-type Transaction struct {
+type transaction struct {
 	tx *sqlx.Tx
 }
 
 func TransactionConnection(tx *sqlx.Tx) sqldb.Connection {
-	return Transaction{tx}
+	return transaction{tx}
 }
 
-func (conn Transaction) Exec(query string, args ...interface{}) error {
+func (conn transaction) Exec(query string, args ...interface{}) error {
 	_, err := conn.tx.Exec(query, args...)
 	return err
 }
 
 // Insert a new row into table using the named columValues.
-func (conn Transaction) Insert(table string, columValues map[string]interface{}) error {
+func (conn transaction) Insert(table string, columValues map[string]interface{}) error {
 	return implhelper.Insert(conn, table, columValues)
 }
 
-func (conn Transaction) InsertStruct(table string, rowStruct interface{}, onlyColumns ...string) error {
+func (conn transaction) InsertStruct(table string, rowStruct interface{}, onlyColumns ...string) error {
 	return implhelper.InsertStruct(conn, table, rowStruct, onlyColumns...)
 }
 
-func (conn Transaction) QueryRow(query string, args ...interface{}) sqldb.RowScanner {
+func (conn transaction) QueryRow(query string, args ...interface{}) sqldb.RowScanner {
 	row := conn.tx.QueryRowx(query, args...)
 	if row.Err() != nil {
 		return sqldb.NewErrRowScanner(row.Err())
@@ -37,7 +37,7 @@ func (conn Transaction) QueryRow(query string, args ...interface{}) sqldb.RowSca
 	return rowScanner{row}
 }
 
-func (conn Transaction) QueryRows(query string, args ...interface{}) sqldb.RowsScanner {
+func (conn transaction) QueryRows(query string, args ...interface{}) sqldb.RowsScanner {
 	rows, err := conn.tx.Queryx(query, args...)
 	if err != nil {
 		return sqldb.NewErrRowsScanner(err)
@@ -45,35 +45,35 @@ func (conn Transaction) QueryRows(query string, args ...interface{}) sqldb.RowsS
 	return &rowsScanner{rows}
 }
 
-func (conn Transaction) Begin() (sqldb.Connection, error) {
+func (conn transaction) Begin() (sqldb.Connection, error) {
 	return nil, sqldb.ErrWithinTransaction
 }
 
-func (conn Transaction) Commit() error {
+func (conn transaction) Commit() error {
 	return conn.tx.Commit()
 }
 
-func (conn Transaction) Rollback() error {
+func (conn transaction) Rollback() error {
 	return conn.tx.Rollback()
 }
 
-func (conn Transaction) Transaction(txFunc func(tx sqldb.Connection) error) error {
+func (conn transaction) Transaction(txFunc func(tx sqldb.Connection) error) error {
 	return sqldb.ErrWithinTransaction
 }
 
-func (conn Transaction) ListenOnChannel(channel string, onNotify sqldb.OnNotifyFunc, onUnlisten sqldb.OnUnlistenFunc) (err error) {
+func (conn transaction) ListenOnChannel(channel string, onNotify sqldb.OnNotifyFunc, onUnlisten sqldb.OnUnlistenFunc) (err error) {
 	return getOrCreateGlobalListener(conn.tx.DriverName()).listenOnChannel(channel, onNotify, onUnlisten)
 }
 
-func (conn Transaction) UnlistenChannel(channel string) (err error) {
+func (conn transaction) UnlistenChannel(channel string) (err error) {
 	return getGlobalListenerOrNil(conn.tx.DriverName()).unlistenChannel(channel)
 }
 
-func (conn Transaction) IsListeningOnChannel(channel string) bool {
+func (conn transaction) IsListeningOnChannel(channel string) bool {
 	return getGlobalListenerOrNil(conn.tx.DriverName()).isListeningOnChannel(channel)
 }
 
-func (conn Transaction) Close() error {
+func (conn transaction) Close() error {
 	conn.Rollback()
 	return nil
 }
