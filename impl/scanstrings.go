b81e152a960ae26d503db1feb1918c138cebcb23
Erik Unger <erik@erikunger.com>
2020-03-08 19:05:25 +0100

    var names

--- impl/scanstrings.go
+++ impl/scanstrings.go
@@ -5,20 +5,20 @@ import "github.com/domonda/go-sqldb"
 // ScanStrings scans the values of a row as strings.
 // Byte slices will be interpreted as strings,
 // nil (SQL NULL) will be converted to an empty string,
-// all other types are converted with fmt.Sprint(src).
+// all other types are converted with fmt.Sprint.
 func ScanStrings(src Row) ([]string, error) {
 	cols, err := src.Columns()
 	if err != nil {
 		return nil, err
 	}
-	s := make([]string, len(cols))
-	p := make([]interface{}, len(cols))
-	for i := range p {
-		p[i] = (*sqldb.StringScannable)(&s[i])
+	row := make([]string, len(cols))
+	args := make([]interface{}, len(cols))
+	for i := range args {
+		args[i] = (*sqldb.StringScannable)(&row[i])
 	}
-	err = src.Scan(p...)
+	err = src.Scan(args...)
 	if err != nil {
 		return nil, err
 	}
-	return s, nil
+	return row, nil
 }
