d4a19e17a722d79ab60d8d027847f9ebe3b8eb16
Erik Unger <erik@erikunger.com>
2021-10-19 08:59:55 +0200

    added imp.SliceScanner

--- impl/scanslice.go
+++ impl/scanslice.go
@@ -2,10 +2,12 @@ package impl
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"reflect"
 	"strings"
+	"time"
 
 	sqldb "github.com/domonda/go-sqldb"
 )
@@ -162,4 +164,140 @@ func SplitArray(array string) ([]string, error) {
 	}
 
 	return elems, nil
+}
+
+type SliceScanner struct {
+	destSlice reflect.Value
+}
+
+func WrapWithSliceScanner(destPtr interface{}) interface{} {
+	v := reflect.ValueOf(destPtr)
+	if v.Elem().Kind() != reflect.Slice || v.Type().Implements(typeOfSQLScanner) {
+		return destPtr
+	}
+	return SliceScanner{destSlice: v.Elem()}
+}
+
+// Scan implements the sql.Scanner interface.
+func (a *SliceScanner) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte:
+		return a.scanString(string(src))
+	case string:
+		return a.scanString(src)
+	default:
+		return fmt.Errorf("can't scan %T as slice", src)
+	}
+}
+
+func (a *SliceScanner) scanString(src string) error {
+	elems, err := SplitArray(src)
+	if err != nil {
+		return err
+	}
+	if len(elems) == 0 {
+		a.destSlice.Set(reflect.Zero(a.destSlice.Type()))
+		return nil
+	}
+	elemType := a.destSlice.Type().Elem()
+	newSlice := reflect.MakeSlice(elemType, len(elems), len(elems))
+	if reflect.PtrTo(elemType).Implements(typeOfSQLScanner) {
+		for i, elem := range elems {
+			err = newSlice.Index(i).Addr().Interface().(sql.Scanner).Scan(elem)
+			if err != nil {
+				return fmt.Errorf("can't scan %q as element %d of slice %s because of %w", elem, i, elemType, err)
+			}
+		}
+	} else {
+		for i, elem := range elems {
+			err = ScanValue(elem, newSlice.Index(i))
+			if err != nil {
+				return fmt.Errorf("can't scan %q as element %d of slice %s because of %w", elem, i, elemType, err)
+			}
+		}
+	}
+	a.destSlice.Set(newSlice)
+	return nil
+}
+
+func ScanValue(src interface{}, dest reflect.Value) error {
+	if dest.Kind() == reflect.Interface {
+		if src != nil {
+			dest.Set(reflect.ValueOf(src))
+		} else {
+			dest.Set(reflect.Zero(dest.Type()))
+		}
+		return nil
+	}
+
+	if dest.Addr().Type().Implements(typeOfSQLScanner) {
+		return dest.Addr().Interface().(sql.Scanner).Scan(src)
+	}
+
+	switch x := src.(type) {
+	case int64:
+		switch dest.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			dest.SetInt(x)
+			return nil
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			dest.SetUint(uint64(x))
+			return nil
+		case reflect.Float32, reflect.Float64:
+			dest.SetFloat(float64(x))
+			return nil
+		}
+
+	case float64:
+		switch dest.Kind() {
+		case reflect.Float32, reflect.Float64:
+			dest.SetFloat(x)
+			return nil
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			dest.SetInt(int64(x))
+			return nil
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			dest.SetUint(uint64(x))
+			return nil
+		}
+
+	case bool:
+		dest.SetBool(x)
+		return nil
+
+	case []byte:
+		switch {
+		case dest.Kind() == reflect.String:
+			dest.SetString(string(x))
+			return nil
+		case dest.Kind() == reflect.Slice && dest.Type().Elem().Kind() == reflect.Uint8:
+			dest.Set(reflect.ValueOf(x))
+			return nil
+		}
+
+	case string:
+		switch {
+		case dest.Kind() == reflect.String:
+			dest.SetString(x)
+			return nil
+		case dest.Kind() == reflect.Slice && dest.Type().Elem().Kind() == reflect.Uint8:
+			dest.Set(reflect.ValueOf([]byte(x)))
+			return nil
+		}
+
+	case time.Time:
+		if srcVal := reflect.ValueOf(src); srcVal.Type().AssignableTo(dest.Type()) {
+			dest.Set(srcVal)
+			return nil
+		}
+
+	case nil:
+		switch dest.Kind() {
+		case reflect.Ptr, reflect.Slice, reflect.Map:
+			dest.Set(reflect.Zero(dest.Type()))
+			return nil
+		}
+	}
+
+	return fmt.Errorf("can't scan %#v as %s", src, dest.Type())
 }
