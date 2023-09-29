be3c0aa9e1c4968a4cd87b9ab4841bc398e599c6
Erik Unger <erik@erikunger.com>
2023-09-29 14:08:45 +0200

    added db.InsertStruct

--- db/query.go
+++ db/query.go
@@ -176,3 +176,10 @@ func QueryStructSlice[S any](ctx context.Context, query string, args ...any) (ro
 	}
 	return rows, nil
 }
+
+// InsertStruct inserts a new row into table using the connection's
+// StructFieldMapper to map struct fields to column names.
+// Optional ColumnFilter can be passed to ignore mapped columns.
+func InsertStruct(ctx context.Context, table string, rowStruct any, ignoreColumns ...sqldb.ColumnFilter) error {
+	return Conn(ctx).InsertStruct(table, rowStruct, ignoreColumns...)
+}
