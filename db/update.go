75400a6243c3b73c91a6e9f0caded11b651aa3c8
Erik Unger <erik@erikunger.com>
2025-07-28 11:17:07 +0200

    move update funcs from db to root package

--- db/update.go
+++ db/update.go
@@ -2,45 +2,18 @@ package db
 
 import (
 	"context"
-	"fmt"
-	"reflect"
-	"slices"
 
 	"github.com/domonda/go-sqldb"
 )
 
 // Update table rows(s) with values using the where statement with passed in args starting at $1.
 func Update(ctx context.Context, table string, values sqldb.Values, where string, args ...any) error {
-	conn := Conn(ctx)
-	queryBuilder := QueryBuilderFuncFromContext(ctx)(conn)
+	var (
+		conn         = Conn(ctx)
+		queryBuilder = QueryBuilderFuncFromContext(ctx)(conn)
+	)
 	return sqldb.Update(ctx, conn, queryBuilder, table, values, where, args...)
 }
-
-// // UpdateReturningRow updates a table row with values using the where statement with passed in args starting at $1
-// // and returning a single row with the columns specified in returning argument.
-// func UpdateReturningRow(ctx context.Context, table string, values Values, returning, where string, args ...any) sqldb.RowScanner {
-// 	if len(values) == 0 {
-// 		return sqldb.RowScannerWithError(fmt.Errorf("UpdateReturningRow table %s: no values passed", table))
-// 	}
-// 	conn := Conn(ctx)
-
-// 	query, vals := buildUpdateQuery(table, values, where, args, conn)
-// 	query += " RETURNING " + returning
-// 	return conn.QueryRow(query, vals...)
-// }
-
-// // UpdateReturningRows updates table rows with values using the where statement with passed in args starting at $1
-// // and returning multiple rows with the columns specified in returning argument.
-// func UpdateReturningRows(ctx context.Context, table string, values Values, returning, where string, args ...any) sqldb.RowsScanner {
-// 	if len(values) == 0 {
-// 		return sqldb.RowsScannerWithError(fmt.Errorf("UpdateReturningRows table %s: no values passed", table))
-// 	}
-// 	conn := Conn(ctx)
-
-// 	query, vals := buildUpdateQuery(table, values, where, args, conn)
-// 	query += " RETURNING " + returning
-// 	return conn.QueryRows(query, vals...)
-// }
 
 // UpdateStruct updates a row in a table using the exported fields
 // of rowStruct which have a `db` tag that is not "-".
@@ -49,31 +22,10 @@ func Update(ctx context.Context, table string, values sqldb.Values, where string
 // The struct must have at least one field with a `db` tag value having a ",pk" suffix
 // to mark primary key column(s).
 func UpdateStruct(ctx context.Context, table string, rowStruct any, options ...sqldb.QueryOption) error {
-	conn := Conn(ctx)
-	queryBuilder := QueryBuilderFuncFromContext(ctx)(conn)
-
-	v := reflect.ValueOf(rowStruct)
-	for v.Kind() == reflect.Ptr && !v.IsNil() {
-		v = v.Elem()
-	}
-	switch {
-	case v.Kind() == reflect.Ptr && v.IsNil():
-		return fmt.Errorf("UpdateStruct of table %s: can't insert nil", table)
-	case v.Kind() != reflect.Struct:
-		return fmt.Errorf("UpdateStruct of table %s: expected struct but got %T", table, rowStruct)
-	}
-
-	columns, vals := sqldb.ReflectStructColumnsAndValues(v, defaultStructReflector, append(options, sqldb.IgnoreReadOnly)...)
-	hasPK := slices.ContainsFunc(columns, func(col sqldb.ColumnInfo) bool {
-		return col.PrimaryKey
-	})
-	if !hasPK {
-		return fmt.Errorf("UpdateStruct of table %s: %s has no mapped primary key field", table, v.Type())
-	}
-
-	query, err := queryBuilder.UpdateColumns(table, columns)
-	if err != nil {
-		return err
-	}
-	return sqldb.Exec(ctx, conn, query, vals...)
+	var (
+		conn         = Conn(ctx)
+		queryBuilder = QueryBuilderFuncFromContext(ctx)(conn)
+		reflector    = GetStructReflector(ctx)
+	)
+	return sqldb.UpdateStruct(ctx, conn, queryBuilder, reflector, table, rowStruct, options...)
 }
--- update.go
+++ update.go
@@ -3,6 +3,8 @@ package sqldb
 import (
 	"context"
 	"fmt"
+	"reflect"
+	"slices"
 )
 
 // Update table rows(s) with values using the where statement with passed in args starting at $1.
@@ -17,6 +19,69 @@ func Update(ctx context.Context, conn Executor, queryBuilder QueryBuilder, table
 	err = conn.Exec(ctx, query, vals...)
 	if err != nil {
 		return WrapErrorWithQuery(err, query, args, queryBuilder)
+	}
+	return nil
+}
+
+// // UpdateReturningRow updates a table row with values using the where statement with passed in args starting at $1
+// // and returning a single row with the columns specified in returning argument.
+// func UpdateReturningRow(ctx context.Context, conn Executor, queryBuilder QueryBuilder, table string, values Values, returning, where string, args ...any) RowScanner {
+// 	if len(values) == 0 {
+// 		return RowScannerWithError(fmt.Errorf("UpdateReturningRow table %s: no values passed", table))
+// 	}
+// 	conn := Conn(ctx)
+
+// 	query, vals := buildUpdateQuery(table, values, where, args, conn)
+// 	query += " RETURNING " + returning
+// 	return conn.QueryRow(query, vals...)
+// }
+
+// // UpdateReturningRows updates table rows with values using the where statement with passed in args starting at $1
+// // and returning multiple rows with the columns specified in returning argument.
+// func UpdateReturningRows(ctx context.Context, conn Executor, queryBuilder QueryBuilder, table string, values Values, returning, where string, args ...any) RowsScanner {
+// 	if len(values) == 0 {
+// 		return RowsScannerWithError(fmt.Errorf("UpdateReturningRows table %s: no values passed", table))
+// 	}
+// 	conn := Conn(ctx)
+
+// 	query, vals := buildUpdateQuery(table, values, where, args, conn)
+// 	query += " RETURNING " + returning
+// 	return conn.QueryRows(query, vals...)
+// }
+
+// UpdateStruct updates a row in a table using the exported fields
+// of rowStruct which have a `db` tag that is not "-".
+// If restrictToColumns are provided, then only struct fields with a `db` tag
+// matching any of the passed column names will be used.
+// The struct must have at least one field with a `db` tag value having a ",pk" suffix
+// to mark primary key column(s).
+func UpdateStruct(ctx context.Context, conn Executor, queryBuilder QueryBuilder, reflector StructReflector, table string, rowStruct any, options ...QueryOption) error {
+	v := reflect.ValueOf(rowStruct)
+	for v.Kind() == reflect.Ptr && !v.IsNil() {
+		v = v.Elem()
+	}
+	switch {
+	case v.Kind() == reflect.Ptr && v.IsNil():
+		return fmt.Errorf("UpdateStruct of table %s: can't insert nil", table)
+	case v.Kind() != reflect.Struct:
+		return fmt.Errorf("UpdateStruct of table %s: expected struct but got %T", table, rowStruct)
+	}
+
+	columns, vals := ReflectStructColumnsAndValues(v, reflector, append(options, IgnoreReadOnly)...)
+	hasPK := slices.ContainsFunc(columns, func(col ColumnInfo) bool {
+		return col.PrimaryKey
+	})
+	if !hasPK {
+		return fmt.Errorf("UpdateStruct of table %s: %s has no mapped primary key field", table, v.Type())
+	}
+
+	query, err := queryBuilder.UpdateColumns(table, columns)
+	if err != nil {
+		return err
+	}
+	err = conn.Exec(ctx, query, vals...)
+	if err != nil {
+		return WrapErrorWithQuery(err, query, vals, queryBuilder)
 	}
 	return nil
 }
