2355785e2f94417fd94eeb18ff37659405a737f0
Erik Unger <erik@erikunger.com>
2026-03-30 13:53:11 +0200

    ci: use test-workspace.sh in GitHub Actions workflow
    Replaces duplicated module-filtering logic in go.yml with
    a single call to test-workspace.sh, which now also builds
    and cleans all modules. Test flags -p 1 -count=1 are baked
    into the script, extra args (like -v) are passed through.

--- test-workspace.sh
+++ test-workspace.sh
@@ -2,18 +2,28 @@
 
 set -e
 
+# Extra arguments are passed to go test, e.g.:
+# ./test-workspace.sh -v
+
 echo "Found modules:"
 go list -f '  {{.Dir}}' -m | grep -v /tools$
 echo ""
 
+echo "Building"
+echo ""
+
+go list -f '{{.Dir}}' -m | grep -v /tools$ | xargs -I {} go build {}/...
+go list -f '{{.Dir}}' -m | grep -v /tools$ | xargs -I {} go clean {}/...
+
+echo ""
 echo "Linting with go vet and gosec"
 echo ""
 
 go list -f '{{.Dir}}' -m | grep -v /tools$ | xargs -I {} go vet {}/...
-go list -f '{{.Dir}}' -m | grep -v /tools$ | grep -v /examples/ | xargs -I {} sh -c 'output=$(go tool gosec {}/... 2>/dev/null) || { printf "%s\n" "$output"; exit 1; }'
+go list -f '{{.Dir}}' -m | grep -v /tools$ | grep -v /examples/ | xargs -I {} go tool gosec {}/...
 
 echo ""
 echo "Testing"
 echo ""
 
-go list -f '{{.Dir}}' -m | grep -v /tools$ | xargs -I {} go test {}/...
+go list -f '{{.Dir}}' -m | grep -v /tools$ | xargs -I {} go test -p 1 -count=1 "$@" {}/...
